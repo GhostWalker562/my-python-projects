326e36b0a9e0f8508f19f6343b8d9bdeb0cf3a4b
Moon <43276017+GhostWalker562@users.noreply.github.com>
2020-07-29 23:15:35 -0700

    Finished Project Dice

---
+++ project_dice.py
@@ -0,0 +1,24 @@
+import random
+
+def main():
+    dice()
+
+
+def dice():
+    isPlaying : bool = True
+    while (isPlaying):
+        print("Type 'r' to roll the dice.")
+        userInput : str = input()
+        if (userInput.lower() == "r"):
+            target : int = random.randint(1,6)
+            print(" -\n|"+ str(target) +"|\n -")
+        else:
+            print("Thanks for rolling!")
+            isPlaying = False
+
+
+
+
+# Defines the main function for the file.
+if __name__ == "__main__":
+    main()
